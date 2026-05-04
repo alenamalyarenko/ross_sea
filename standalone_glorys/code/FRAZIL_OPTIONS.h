2e0892d2196bd009c2032c2b0ce9f32f933e4677
Alena Malyarenko <alena.malyarenko@canterbury.ac.nz>
2026-05-04 13:43:01 +1200

    glorys in 69k

---
+++ standalone_glorys/code/FRAZIL_OPTIONS.h
@@ -0,0 +1,32 @@
+C CPP options file for FRAZIL
+C Use this file for selecting options within package "Frazil"
+
+#ifndef FRAZIL_OPTIONS_H
+#define FRAZIL_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_FRAZIL
+
+C Three options to prevent Ice Shelf Water, which escapes from cavities, from forming sea ice. This process, while realistic, is inherently unstable as it causes isolated patches of sea ice tens of metres thick.
+
+C Calculate supercooling of potential temperature with respect to the surface freezing point, NOT in-situ temperature with respect to the depth-dependent freezing point.
+#define FRAZIL_TFREEZE_SFC
+
+C Instead of moving the supercooling to the surface layer, just remove it completely. Send it to space!
+#undef FRAZIL_ZAP
+
+C Don't do anything to supercooled water in cavities.
+#define FRAZIL_IGNORE_CAVITIES
+
+C Slow release of supercooled water in cavities (~seaicefrazil)
+#undef FRAZIL_SLOW_RISE
+
+
+
+#endif /* ALLOW_FRAZIL */
+#endif /* FRAZIL_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
